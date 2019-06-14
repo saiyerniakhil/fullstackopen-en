ad95c2aeae55bc2f50011dcffce11863148980cf
saiyerniakhil <saiyerniakhil>
2019-06-14 13:03:33 +0530

    Unicafe Step6

--- part 1/unicafe/src/index.js
+++ part 1/unicafe/src/index.js
@@ -2,61 +2,86 @@ import React, {useState} from 'react';
 import ReactDOM from 'react-dom';
 // import styled from 'styled-components'
 
-const Stat = ({text, value}) => {
-    
+const Statistic = ({text, value,all,sym}) => {
+
     if( isNaN(value)) {
         return (
             <>
-                {text} 0
+                <td>{text}</td> <td>0</td>
             </>    
         )
     }
     else {
-
         return (
-                <>
-                    {text} {value}
-                </>
-            )
-        }
+            <>
+            <td>{text}</td>  <td>{value} {sym}</td>
+            </>
+        )
+    }
 }
  
+const Statistics = (props) => {
+
+    const {good,bad,neutral,all} = props
+    return (
+        <>    
+            <table>
+            <tr>
+            <Statistic text="good" value={good} /> </tr>
+            <tr><Statistic text="Neutral" value={neutral} /> </tr>                  
+            <tr><Statistic text="Bad" value={bad} /></tr>
+            <tr><Statistic text="all" value={good+neutral+bad} /></tr>
+            <tr><Statistic text="average" value={all/(good+neutral+bad)} /> </tr>
+            <tr><Statistic text="positive" value={(good/(good+neutral+bad))*100} sym="%" /></tr>
+            </table>
+        </>
+    )
+}
 const App = () => {
 
     const [good,setGood] = useState(0)
     const [neutral,setNeutral] = useState(0)
     const [bad,setBad] = useState(0)
     const [all,setAll] = useState(0)
+    const [total,setTotals] = useState(0)
 
     const handleGoodClick = () => {
         setGood(good+1)
         setAll(all+1)
+        setTotals(total+1)
     }
     const handleBadClick = () => {
         setBad(bad+1)
         setAll(all-1)
+        setTotals(total+1)
     }
     const handleNeutralClick = () => {
         setNeutral(neutral+1)
         setAll(all+0)
+        setTotals(total+1)
     } 
 
+    
     return (
-       <div>    
+    <div>    
         <button onClick={handleGoodClick}>good</button> 
         <button onClick={handleNeutralClick}>neutral</button>
         <button onClick={handleBadClick}>bad</button>
+        
         <h1>Statistics</h1>
-        <Stat text="good" value={good}/> <br/>        
-        <Stat text="Neutral" value={neutral}/> <br/>
-        <Stat text="Bad" value={neutral}/> <br/>
-        <Stat text="all" value={good+neutral+bad}/> <br/>
-        <Stat text="average" value={all/(good+neutral+bad)}/> <br/>
-        <Stat text="positive" value={(good/(good+neutral+bad))*100}/>
+        {total?
+        <div>
+        <Statistics good={good} bad={bad} neutral={neutral} all={all}/>
+        </div>
+        :
+        <div>No Feedback Given</div>
+        }
         
         
-       </div>    
+        
+    </div>    
     )
+    
 }
 
 ReactDOM.render(<App/>,document.querySelector('#root'))
