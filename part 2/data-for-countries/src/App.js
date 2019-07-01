e842a6d05ba71e9027240f65f6cd75537e1b68b2
saiyerniakhil <makhil123@yahoo.in>
2019-07-01 16:44:50 +0530

    Add Countries Data App

--- part 2/data-for-countries/src/App.js
+++ part 2/data-for-countries/src/App.js
@@ -1,26 +1,59 @@
-import React from 'react';
-import logo from './logo.svg';
+import React,{useEffect, useState} from 'react';
+import axios from 'axios';
+// import logo from './logo.svg';
 import './App.css';
 
-function App() {
+
+const App = () => {
+
+  const [countriesData,setCountriesData] = useState([])
+  const [country, setCountry] = useState([])
+  const [foundCountry,setFoundCountry] = useState([])
+  // const [countriesLength,setCountriesLength] = useState(0)
+
+  const hook = () => {
+    axios.get("https://restcountries.eu/rest/v2/all")
+    .then(response => {
+      console.log("data fetched")
+      setCountriesData(countriesData => countriesData.concat(response.data))
+      // console.log(countriesData)
+    })
+  }
+  useEffect(hook,[])
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+  }
+
+  const findCountry = (val) => {
+    var data = countriesData.filter(d => {
+      return (d.name).toLowerCase().includes(val)
+    })
+    return data
+  }
+
+  const handleInputChange = (e) => {
+    setCountry(e.target.value)
+    let country = (e.target.value).toLowerCase() 
+    const x = findCountry(country)
+    // console.log(x)
+    setFoundCountry(x)
+  }
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>  
+      <form onSubmit={handleSubmit}>
+        <input type="text" value={country} onChange={handleInputChange}/>
+        <button type="submit">find</button>
+      </form> 
+      <ul>
+        {
+          foundCountry.length > 3 ? <li>Too many matches, please specify another</li> : foundCountry.map(item => (<li key={item.numericCode}>{item.name}</li>))
+        }
+      </ul>
     </div>
-  );
+  )
 }
 
 export default App;
