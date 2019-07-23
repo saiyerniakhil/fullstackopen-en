cfaa7385dab5b0fbd18bcba53115dadaeb728002
saiyerniakhil <makhil123@yahoo.in>
2019-07-23 21:31:19 +0530

    uibuild

---
+++ part 3/phonebook-backend/build/precache-manifest.53184f031316b28128b1f9992f49ca5a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6fdb64f312fac9a1272b9915e098190f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0a8bdfde81446535a39d",
+    "url": "/static/css/main.d30398fd.chunk.css"
+  },
+  {
+    "revision": "6a19a0beea039a280f26",
+    "url": "/static/js/2.3c62b441.chunk.js"
+  },
+  {
+    "revision": "0a8bdfde81446535a39d",
+    "url": "/static/js/main.e0dc9520.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
