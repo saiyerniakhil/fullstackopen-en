c3d065131941f89a8c70203dce5cf8dd09b28906
saiyerniakhil <makhil123@yahoo.in>
2019-07-23 19:51:21 +0530

    update backend

--- part 3/phonebook-backend/index.js
+++ part 3/phonebook-backend/index.js
@@ -17,7 +17,7 @@ const errorHandler = (error,request,response,next) => {
 
   if(error.name == 'CastError' && error.kind== 'ObjectId') {
     return response.status(400).send({error: 'ill formatted id'})
-  } (error.name == 'ValidationError') {
+  } else if(error.name == 'ValidationError') {
     return response.status(400).send({error: error.message})
   }
 
